b72c9f3645029a37d4f2cf99557214edec748661
SorrowOfMind <sortoflifecraze@gmail.com>
2022-08-02 12:46:59 +0200

    bubble class, bubble handler

--- dist/bubble.js
+++ dist/bubble.js
@@ -7,5 +7,38 @@ var Canvas = /** @class */ (function () {
     }
     return Canvas;
 }());
+var Bubble = /** @class */ (function () {
+    function Bubble(color, speed) {
+        this.radiusMin = 20;
+        this.radiusMax = 150;
+        this.color = color;
+        this.velY = this.randomizeValue(0.001, 0.002) + speed;
+        this.velX = this.randomizeValue(-2, 4);
+        this.isAlive = true;
+        this.radius = this.randomizeValue(this.radiusMin, this.radiusMax);
+        this.velRadius = 0;
+        this.coords = {
+            x: this.randomizeValue(0, window.innerWidth),
+            y: this.randomizeValue(window.innerHeight + this.radius, 50)
+        };
+    }
+    Bubble.prototype.randomizeValue = function (min, max) {
+        return (Math.random() * (max - min)) + min;
+    };
+    return Bubble;
+}());
+var BubbleHandler = {
+    bubbles: [],
+    bubblesBg: [],
+    addBubble: function (bubble, bubbleType) {
+        if (bubbleType === void 0) { bubbleType = 'bubble'; }
+        if (bubbleType === 'bubble') {
+            this.bubbles.push(bubble);
+        }
+        else {
+            this.bubblesBg.push(bubble);
+        }
+    }
+};
 var cvsBg = new Canvas(document.getElementById('bg'), window.innerWidth, window.innerHeight);
 var cvs = new Canvas(document.getElementById('cvs'), window.innerWidth, window.innerHeight);
--- src/bubble.ts
+++ src/bubble.ts
@@ -7,6 +7,25 @@ interface CanvasContructor {
     new (cvs: HTMLCanvasElement, width: number, height: number): CanvasInterface;
 }
 
+interface BubbleInterface {
+    color: string;
+    velY: number;
+    velX: number;
+    isAlive: boolean;
+    coords: Coords;
+}
+
+interface BubbleHandlerInterface {
+    bubbles: [];
+    bubblesBg: [];
+    addBubble(bubble: Bubble, bubbleType: string): void;
+}
+
+type Coords = {
+    x: number,
+    y: number
+}
+
 const Canvas: CanvasContructor = class Canvas implements CanvasInterface {
     public cvs: HTMLCanvasElement;
     public ctx: CanvasRenderingContext2D;
@@ -16,6 +35,55 @@ const Canvas: CanvasContructor = class Canvas implements CanvasInterface {
         this.ctx = cvs.getContext('2d');
         this.cvs.width = width;
         this.cvs.height = height;
+    }
+}
+
+class Bubble {
+    public color: string;
+    public velY: number;
+    public velX: number;
+    public isAlive: boolean;
+    public coords: Coords;
+
+    private radius: number;
+    private velRadius: number; //how fast will it shrink
+
+    private readonly radiusMin = 20; 
+    private readonly radiusMax = 150; 
+
+    constructor(color: string, speed: number) {
+        this.color = color;
+
+        this.velY = this.randomizeValue(0.001, 0.002) + speed;
+        this.velX = this.randomizeValue(-2, 4);
+
+        this.isAlive = true;
+
+        this.radius = this.randomizeValue(this.radiusMin, this.radiusMax);
+        this.velRadius = 0;
+
+        this.coords = {
+            x: this.randomizeValue(0, window.innerWidth),
+            y: this.randomizeValue(window.innerHeight + this.radius, 50)
+        }
+
+    }
+
+    randomizeValue(min: number, max: number) {
+        return (Math.random() * (max - min)) + min;
+    }
+}
+
+const BubbleHandler: BubbleHandlerInterface = {
+    bubbles: [],
+    bubblesBg: [],
+
+    addBubble(bubble: Bubble, bubbleType = 'bubble') {
+        if (bubbleType === 'bubble') {
+            this.bubbles.push(bubble);
+        } else {
+            this.bubblesBg.push(bubble);
+        }
     }
 }
 
