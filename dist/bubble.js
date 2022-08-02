3b7b25d3590187d6d501ab1220a26d03abe5f473
SorrowOfMind <sortoflifecraze@gmail.com>
2022-08-02 15:53:45 +0200

    bubbles handler

--- dist/bubble.js
+++ dist/bubble.js
@@ -9,21 +9,40 @@ var Canvas = /** @class */ (function () {
 }());
 var Bubble = /** @class */ (function () {
     function Bubble(color, speed) {
-        this.radiusMin = 20;
+        this.radiusMin = 40;
         this.radiusMax = 150;
         this.color = color;
-        this.velY = this.randomizeValue(0.001, 0.002) + speed;
-        this.velX = this.randomizeValue(-2, 4);
+        this.velY = this.randomizeValue(0.001, 0.0012) + speed;
+        this.velX = this.randomizeValue(-2, 2);
         this.isAlive = true;
         this.radius = this.randomizeValue(this.radiusMin, this.radiusMax);
         this.velRadius = 0;
         this.coords = {
             x: this.randomizeValue(0, window.innerWidth),
-            y: this.randomizeValue(window.innerHeight + this.radius, 50)
+            y: this.randomizeValue(window.innerHeight + this.radius, window.innerHeight + this.radius - 40)
         };
     }
     Bubble.prototype.randomizeValue = function (min, max) {
         return (Math.random() * (max - min)) + min;
+    };
+    Bubble.prototype.draw = function (ctx) {
+        ctx.beginPath();
+        ctx.arc(this.coords.x, this.coords.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    };
+    Bubble.prototype.update = function () {
+        // console.log('update', this.velY)
+        this.velY += 0.00001;
+        this.velRadius += 0.02;
+        this.coords.x += this.velX;
+        this.coords.y -= this.velY;
+        if (this.radius > 1) {
+            this.radius -= this.velRadius;
+        }
+        if (this.radius <= 1) {
+            this.isAlive = false;
+        }
     };
     return Bubble;
 }());
@@ -38,7 +57,48 @@ var BubbleHandler = {
         else {
             this.bubblesBg.push(bubble);
         }
+    },
+    handleBubbles: function () {
+        for (var i = this.bubbles.length - 1; i >= 0; i--) {
+            this.bubbles[i].update();
+            if (!this.bubbles[i].isAlive) {
+                this.bubbles.splice(i, 1);
+            }
+        }
+        for (var i = this.bubblesBg.length - 1; i >= 0; i--) {
+            this.bubblesBg[i].update();
+            if (!this.bubblesBg[i].isAlive) {
+                this.bubblesBg.splice(i, 1);
+            }
+        }
+        if (this.bubbles.length < window.innerWidth / 4) {
+            this.addBubble(new Bubble('rgba(249, 248, 113, 1)', 1.8));
+        }
+        if (this.bubblesBg.length < window.innerWidth / 12) {
+            this.addBubble(new Bubble('rgba(227,176,225,1)', 4.5), 'bg');
+        }
     }
 };
 var cvsBg = new Canvas(document.getElementById('bg'), window.innerWidth, window.innerHeight);
 var cvs = new Canvas(document.getElementById('cvs'), window.innerWidth, window.innerHeight);
+function animate() {
+    cvs.ctx.clearRect(0, 0, cvs.cvs.width, cvs.cvs.height);
+    cvsBg.ctx.clearRect(0, 0, cvs.cvs.width, cvs.cvs.height);
+    BubbleHandler.handleBubbles();
+    for (var i = BubbleHandler.bubbles.length - 1; i >= 0; i--) {
+        BubbleHandler.bubbles[i].draw(cvs.ctx);
+    }
+    for (var i = BubbleHandler.bubblesBg.length - 1; i >= 0; i--) {
+        BubbleHandler.bubblesBg[i].draw(cvsBg.ctx);
+    }
+    requestAnimationFrame(animate);
+}
+window.addEventListener('load', animate);
+window.addEventListener('resize', function () {
+    cvs.cvs.width = this.innerWidth;
+    cvs.cvs.height = this.innerHeight;
+    cvsBg.cvs.width = this.innerWidth;
+    cvsBg.cvs.height = this.innerHeight;
+    BubbleHandler.bubbles = [];
+    BubbleHandler.bubblesBg = [];
+});
--- src/bubble.ts
+++ src/bubble.ts
@@ -13,12 +13,15 @@ interface BubbleInterface {
     velX: number;
     isAlive: boolean;
     coords: Coords;
+    update(): void;
+    draw(ctx: CanvasRenderingContext2D): void;
 }
 
 interface BubbleHandlerInterface {
-    bubbles: [];
-    bubblesBg: [];
+    bubbles: (Bubble | null)[];
+    bubblesBg: (Bubble | null)[];
     addBubble(bubble: Bubble, bubbleType: string): void;
+    handleBubbles(): void;
 }
 
 type Coords = {
@@ -38,7 +41,7 @@ const Canvas: CanvasContructor = class Canvas implements CanvasInterface {
     }
 }
 
-class Bubble {
+class Bubble implements BubbleInterface {
     public color: string;
     public velY: number;
     public velX: number;
@@ -48,14 +51,14 @@ class Bubble {
     private radius: number;
     private velRadius: number; //how fast will it shrink
 
-    private readonly radiusMin = 20; 
+    private readonly radiusMin = 40; 
     private readonly radiusMax = 150; 
 
     constructor(color: string, speed: number) {
         this.color = color;
 
-        this.velY = this.randomizeValue(0.001, 0.002) + speed;
-        this.velX = this.randomizeValue(-2, 4);
+        this.velY = this.randomizeValue(0.001, 0.0012) + speed;
+        this.velX = this.randomizeValue(-2, 2);
 
         this.isAlive = true;
 
@@ -64,13 +67,35 @@ class Bubble {
 
         this.coords = {
             x: this.randomizeValue(0, window.innerWidth),
-            y: this.randomizeValue(window.innerHeight + this.radius, 50)
+            y: this.randomizeValue(window.innerHeight + this.radius, window.innerHeight + this.radius - 40)
         }
 
     }
 
     randomizeValue(min: number, max: number) {
         return (Math.random() * (max - min)) + min;
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.arc(this.coords.x, this.coords.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        // console.log('update', this.velY)
+        this.velY += 0.00001;
+        this.velRadius += 0.02;
+        this.coords.x += this.velX;
+        this.coords.y -= this.velY;
+        if (this.radius > 1) {
+            this.radius -= this.velRadius;
+        } 
+        if (this.radius <= 1) {
+            this.isAlive = false;
+
+        }
     }
 }
 
@@ -84,9 +109,61 @@ const BubbleHandler: BubbleHandlerInterface = {
         } else {
             this.bubblesBg.push(bubble);
         }
+    },
+
+    handleBubbles() {
+        for(let i = this.bubbles.length - 1; i >= 0; i--) {
+            this.bubbles[i].update();
+            if (!this.bubbles[i].isAlive) {
+                this.bubbles.splice(i, 1);
+            }
+        }
+        for(let i = this.bubblesBg.length - 1; i >= 0; i--) {
+            this.bubblesBg[i].update();
+            if (!this.bubblesBg[i].isAlive) {
+                this.bubblesBg.splice(i, 1);
+            }
+        }
+
+        if (this.bubbles.length < window.innerWidth / 4) {
+            this.addBubble(new Bubble('rgba(249, 248, 113, 1)', 1.8));
+        }
+        if (this.bubblesBg.length < window.innerWidth / 12) {
+            this.addBubble(new Bubble('rgba(227,176,225,1)', 4.5), 'bg');
+        }
     }
 }
 
 
 let cvsBg = new Canvas(document.getElementById('bg') as HTMLCanvasElement, window.innerWidth, window.innerHeight);
 let cvs = new Canvas(document.getElementById('cvs') as HTMLCanvasElement,  window.innerWidth, window.innerHeight);
+
+function animate() {
+    cvs.ctx.clearRect(0, 0, cvs.cvs.width, cvs.cvs.height);
+    cvsBg.ctx.clearRect(0, 0, cvs.cvs.width, cvs.cvs.height);
+
+    BubbleHandler.handleBubbles();
+
+    for(let i = BubbleHandler.bubbles.length - 1; i >= 0; i--) {
+        BubbleHandler.bubbles[i].draw(cvs.ctx);
+    }
+
+    for(let i = BubbleHandler.bubblesBg.length - 1; i >= 0; i--) {
+        BubbleHandler.bubblesBg[i].draw(cvsBg.ctx);
+    }
+    
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('load', animate);
+
+window.addEventListener('resize', function() {
+    cvs.cvs.width = this.innerWidth;
+    cvs.cvs.height = this.innerHeight;
+    cvsBg.cvs.width = this.innerWidth;
+    cvsBg.cvs.height = this.innerHeight;
+
+    BubbleHandler.bubbles = [];
+    BubbleHandler.bubblesBg = [];
+});
+
